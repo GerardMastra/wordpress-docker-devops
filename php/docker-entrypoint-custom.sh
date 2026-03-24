697a38a5f174e839c2036c6c2ec1956bcec9db95
Gerard <geramastra@gmail.com>
2026-03-23 22:29:37 -0300

    Versión v1.3.2 – CI/CD prueba

--- php/docker-entrypoint-custom.sh
+++ php/docker-entrypoint-custom.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-# Generar wp-config.php desde el template usando las variables de entorno de Docker
-echo "🔧 Generando wp-config.php dinámicamente..."
-export VARS='$WP_DB_NAME,$WP_DB_USER,$WP_DB_PASSWORD,$WP_DB_HOST,$WP_HOME,$WP_SITEURL'
-envsubst "$VARS" < /usr/src/wordpress/wp-config.php.template > /var/www/html/wp-config.php
+# Generar wp-config.php desde el template
+echo "🔧 Generando wp-config.php..."
+envsubst '$WP_DB_NAME,$WP_DB_USER,$WP_DB_PASSWORD,$WP_DB_HOST,$WP_HOME,$WP_SITEURL' < /usr/src/wordpress/wp-config.php.template > /var/www/html/wp-config.php
 
-# Asegurar permisos
 chown www-data:www-data /var/www/html/wp-config.php
 
-# Ejecutar el entrypoint original de la imagen oficial de WordPress
+# Ejecutar el entrypoint original de la imagen
 exec docker-entrypoint.sh "$@"
